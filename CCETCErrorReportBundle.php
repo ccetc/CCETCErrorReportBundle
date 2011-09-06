20d2884632b196a8e3725f5f8f81cc88f5a21065
Pat Haggerty <haggertypat@gmail.com>
2011-09-05 20:57:48 -0400

    styles added, base_layout var added

---
+++ CCETCErrorReportBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CCETC\ErrorReportBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CCETCErrorReportBundle extends Bundle
+{
+}
--- Controller/ErrorReportController.php
+++ Controller/ErrorReportController.php
@@ -5,69 +5,71 @@ namespace CCETC\ErrorReportBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 use Sonata\AdminBundle\Controller\CoreController;
 
 class ErrorReportController extends CoreController
 {
 
-  public function errorReportAction()
-  {
-  	// TODO: get support email from config
+    public function errorReportAction($baseLayout)
+    {
+        // TODO: get support email from config
 //    $supportEmail = $this->get('gjgny')->supportEmail;
-	$supportEmail = "haggertypat@gmail.com";
+        $supportEmail = "haggertypat@gmail.com";
 
-    $form = $this->createFormBuilder()
-            ->add('email', 'text', array(
-                'label' => 'Enter your e-mail address if you would like to be contacted about this error.',
-                'required' => false
-            ))
-            ->add('content', 'textarea', array('label' => 'Please enter a description of the problem that you are experiencing.'))
-            ->getForm();
+        $form = $this->createFormBuilder()
+                ->add('email', 'text', array(
+                    'label' => 'Enter your e-mail address if you would like to be contacted about this error.',
+                    'required' => false
+                ))
+                ->add('content', 'textarea', array('label' => 'Please enter a description of the problem that you are experiencing.'))
+                ->getForm();
 
-    $request = $this->get('request');
-    
-    if($request->getMethod() == 'POST')
-    {
-      $form->bindRequest($request);
+        $request = $this->get('request');
 
-      if($form->isValid())
-      {
-        $session = $this->getRequest()->getSession();
-        $data = $form->getData();
+        if($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
 
-        $errorReport = new \CCETC\ErrorReportBundle\Entity\ErrorReport();
-        $errorReport->setContent($data['content']);
-        $errorReport->setWriterEmail($data['email']);
-        $errorReport->setDatetimeReported(new \DateTime());
+            if($form->isValid())
+            {
+                $session = $this->getRequest()->getSession();
+                $data = $form->getData();
 
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($errorReport);
-        $em->flush();
+                $errorReport = new \CCETC\ErrorReportBundle\Entity\ErrorReport();
+                $errorReport->setContent($data['content']);
+                $errorReport->setWriterEmail($data['email']);
+                $errorReport->setDatetimeReported(new \DateTime());
 
-        $session->setFlash('adminMessage', 'Your report has been submitted.  Thank you');
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($errorReport);
+                $em->flush();
 
-		// TODO: return home
-        return $this->render('CCETCErrorReportBundle:submitErrorReport.html.twig', array(
-            'errorReportForm' => $form->createView(),
-            'supportEmail' => $supportEmail
-        ));
-      }
-      else
-      {
-        return $this->render('CCETCErrorReportBundle:submitErrorReport.html.twig', array(
-            'errorReportForm' => $form->createView(),
-            'supportEmail' => $supportEmail
-        ));
-      }
+                $session->setFlash('adminMessage', 'Your report has been submitted.  Thank you');
+
+                // TODO: return home
+                return $this->render('CCETCErrorReportBundle:ErrorReport:submit.html.twig', array(
+                    'errorReportForm' => $form->createView(),
+                    'supportEmail' => $supportEmail,
+                    'base_layout' => $baseLayout
+                ));
+            }
+            else
+            {
+                return $this->render('CCETCErrorReportBundle:ErrorReport:submit.html.twig', array(
+                    'errorReportForm' => $form->createView(),
+                    'supportEmail' => $supportEmail,
+                    'base_layout' => $baseLayout
+                ));
+            }
+        }
+        else
+        {
+            return $this->render('CCETCErrorReportBundle:ErrorReport:submit.html.twig', array(
+                'errorReportForm' => $form->createView(),
+                'supportEmail' => $supportEmail,
+                'base_layout' => $baseLayout
+            ));
+        }
     }
-    else
-    {
-      return $this->render('CCETCErrorReportBundle:submitErrorReport.html.twig', array(
-          'errorReportForm' => $form->createView(),
-          'supportEmail' => $supportEmail
-      ));
-    }
-  }
 
 }
