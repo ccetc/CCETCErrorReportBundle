7e926cdf04f2f8d129692a993f2c97b57121906b
Pat Haggerty <haggertypat@gmail.com>
2011-12-08 15:08:59 -0500

    emails added

--- Controller/ErrorReportController.php
+++ Controller/ErrorReportController.php
@@ -74,7 +74,15 @@ class ErrorReportController extends Controller
                 
                 $session->setFlash($flash, 'Your report has been submitted.  Thank you');
 
-                return $this->redirect($this->generateUrl($redirect));
+                if(!$this->get('security.context')->isGranted('ROLE_USER'))
+                {                
+                    return $this->redirect($this->generateUrl('fos_user_security_login'));
+                }
+                else
+                {
+                    return $this->redirect($this->generateUrl($redirect));                    
+                }
+                
             }
             else
             {
